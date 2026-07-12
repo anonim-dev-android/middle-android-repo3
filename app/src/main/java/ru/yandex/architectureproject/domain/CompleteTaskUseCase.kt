6adbd30cff92ab428ef4d6577197373594f8ee08
Anastasia <asya.vishnewsckaya@yandex.ru>
2026-07-12 22:43:26 +0300

    completed all tasks

--- app/src/main/java/ru/yandex/architectureproject/domain/CompleteTaskUseCase.kt
+++ app/src/main/java/ru/yandex/architectureproject/domain/CompleteTaskUseCase.kt
@@ -1,12 +1,21 @@
 package ru.yandex.architectureproject.domain
 
+import kotlinx.coroutines.delay
 import ru.yandex.architectureproject.data.repository.TaskRepository
 
 class CompleteTaskUseCase(
     private val repository: TaskRepository,
+
 ) {
+    private val autodeleteDelayMs = 10_000L
+
+    /**
+     * // [Задание 3] автоудаление задачи через [autodeleteDelayMs] секунд после выполнения
+     */
     suspend operator fun invoke(taskId: Int) {
         repository.completeTask(taskId)
-        // TODO: Здесь будет автоудаление задачи
+        delay(autodeleteDelayMs)
+        repository.deleteTask(taskId)
     }
+
 }
--- app/src/main/java/ru/yandex/architectureproject/presentation/state/TaskAction.kt
+++ app/src/main/java/ru/yandex/architectureproject/presentation/state/TaskAction.kt
@@ -1,5 +1,29 @@
 package ru.yandex.architectureproject.presentation.state
 
+/**
+ * // [Задание 1]
+ * события, которые происходят в UI или приходят от внешних источников
+ */
 sealed class TaskAction {
-    // TODO: Здесь должны быть действия (загрузка заданий, добавление задания, обновление статуса задания (сделано/не сделано) и удаление задания)
+    /** Загрузка заданий — для получения списка задач */
+    data object LoadTasks : TaskAction()
+
+    /**
+     * Добавление задания — для создания новой задачи
+     * @param text текст задания, которое нужно добавить
+     * */
+    data class AddTask(val text: String) : TaskAction()
+
+    /**
+     * Обновление статуса задания — чтобы помечать задачу как выполненную или невыполненную
+     * @param id id задания
+     * @param isDone сделано/не сделано
+     * */
+    data class UpdateTaskStatus(val id: Int, val isDone: Boolean) : TaskAction()
+
+    /**
+     * Удаление задания — для удаления задач
+     * @param id id задания, которое надо удалить
+     * */
+    data class DeleteTask(val id: Int) : TaskAction()
 }
