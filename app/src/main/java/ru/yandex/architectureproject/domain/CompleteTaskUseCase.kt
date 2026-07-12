6ab591991a589ae8593f784a3c11ae623eb21f25
Anastasia <asya.vishnewsckaya@yandex.ru>
2026-07-12 22:59:16 +0300

    Revert changes

--- app/src/main/java/ru/yandex/architectureproject/domain/CompleteTaskUseCase.kt
+++ app/src/main/java/ru/yandex/architectureproject/domain/CompleteTaskUseCase.kt
@@ -1,21 +1,12 @@
 package ru.yandex.architectureproject.domain
 
-import kotlinx.coroutines.delay
 import ru.yandex.architectureproject.data.repository.TaskRepository
 
 class CompleteTaskUseCase(
     private val repository: TaskRepository,
-
 ) {
-    private val autodeleteDelayMs = 10_000L
-
-    /**
-     * // [Задание 3] автоудаление задачи через [autodeleteDelayMs] секунд после выполнения
-     */
     suspend operator fun invoke(taskId: Int) {
         repository.completeTask(taskId)
-        delay(autodeleteDelayMs)
-        repository.deleteTask(taskId)
+        // TODO: Здесь будет автоудаление задачи
     }
-
 }
